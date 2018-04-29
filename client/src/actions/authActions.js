136c87b97bac6e803b651a1a2ec9bb9f3d95cb25
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2018-04-29 11:50:05 +0300

    New package lock file.

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -9,7 +9,7 @@ import {
 import { AsyncStorage } from 'react-native';
 import axios from 'axios';
 
-const ROOT_URL = 'https://localhost:5000';
+const ROOT_URL = 'https://som-app-server.herokuapp.com';
 
 export const emailChanged = text => {
     return {
